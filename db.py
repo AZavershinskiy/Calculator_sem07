bb042528a2a60397321738ef82b515648e6257b1
A_Zavershinskiy_Win7 <yeah-men@mail.ru>
2022-11-16 16:19:03 +0200

    Rename file for logs.txt

--- db.py
+++ db.py
@@ -1,10 +1,10 @@
 def saving_numbers(a, b, operation):
-    path = 'numbers.txt'
+    path = 'Logs.txt'
     with open(path, 'a') as file:
         file.write(f'{a}{operation}{b}\n')
 
 
 def reading_number():
-    path = 'numbers.txt'
+    path = 'Logs.txt'
     with open(path, 'r') as file:
         return file.readlines()[-1]
