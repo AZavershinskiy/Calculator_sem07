4828621db4ee34f27fac4734b459a257ff98f790
NadezhdaBrin <111063803+NadezhdaBrin@users.noreply.github.com>
2022-11-23 11:06:20 +0300

    Update db.py
    Uptate calculator

--- db.py
+++ db.py
@@ -1,14 +1,17 @@
-from datetime import datetime as dt
-from time import time
+import datetime
+
 
 def saving_numbers(a, b, operation, result):
-    time = dt.now().strftime()
     path = 'Logs.txt'
     with open(path, 'a') as file:
-        file.write(f'{a}{operation}{b}={result}\n')
+        file.write(f'{now_log}   {a}{operation}{b}={result}\n')
 
 
 def reading_number():
     path = 'Logs.txt'
     with open(path, 'r') as file:
         return file.readlines()[-1]
+
+
+now = datetime.datetime.now()
+now_log = now.strftime('%y-%m-%d %H:%M:%S')
